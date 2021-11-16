71494cd38c912f3fc8434e2b4269369c08c5bdf9
n21023 <n21023@std.it-college.ac.jp>
2021-11-16 05:58:49 +0000

    react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useState } from 'react'
+const ButtonComponent = props => {
+  const [text, setText] = useState('Click me')
+  const f = event => setText('Clicked')
+  return (
+    <button
+      onClick={f}
+    >
+      {text}
+    </button>
+  )
+}
+
 const App = props => {
+  const [text, setText] = useState('Click me')
+  const handleClick = event => setText('Clicked')
   return (
     <>
-      <button>Click me!</button>
+      <ButtonComponent
+        text={text} handleClick={handleClick}
+      />
     </>
   )
 }
