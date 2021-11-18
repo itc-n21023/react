013257460b9810dc720fef0c5783e434541f4391
n21023 <n21023@std.it-college.ac.jp>
2021-11-18 07:31:11 +0000

    vol.2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { useState } from 'react'
 import './App.css'
 
+const values = [
+  { id: 1, item: 'HTML' },
+  { id: 2, item: 'CSS' },
+  { id: 3, item: 'JavaScript' }
+]
+const SelectItems = values.map((value) => {
+  return (
+    <option value={value.item} key={value.id}>
+      {value.item}
+    </option>
+  )
+})
 const InputSelectBox = () => {
-  const [selectedValue, setSelectedValue] = useState('HTML')
+  const [selectedValue, setSelectedValue] =
+    useState(values[0]['item'])
   const handleChange = (e) => {
     setSelectedValue(e.target.value)
   }
   return (
     <div className='App'>
       <p>
-         現在選択されている値： 
+        現在選択されている値： 
         <b>{selectedValue}</b>
       </p>
       <select value={selectedValue} onChange={handleChange}>
-        <option value='HTML'>HTML</option>
-        <option value='CSS'>CSS</option>
-        <option value='JavaScript'>JavaScript</option>
+        {SelectItems}
       </select>
     </div>
   )
