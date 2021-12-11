97b973c5c766a29eb18ca2f524a50d052d9358a4
n21023 <n21023@std.it-college.ac.jp>
2021-12-11 05:52:18 +0000

    ep1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react'
-import InputSelectBox from './selectbox'
+import GoogleMaps from './Component/GoogleMaps.js'
 import './App.css'
 
 export default function App () {
   const [values, setValues] = useState([])
+  const 
   useEffect(() =>
-    window.fetch('http://localhost:3000/values.json')
+    window.fetch('http://localhost:3000')
       .then(res => res.json())
       .then(json => json.values)
       .then(values => setValues(values))
   , [])
-  return <InputSelectBox values={values} />
+  return <GoogleMaps values={values} />
 }
